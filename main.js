c57afa858e8b84d084dc4e1c1773383bb1597892
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-23 15:51:59 +0300

    first

---
+++ main.js
@@ -0,0 +1,30 @@
+// 1
+// function getPromise (){
+//     return  new Promise ((resolve,reject)=>{
+//         const Hour = new Date().getHours()
+
+//         if(Hour<11){
+//             resolve(`good luck good morning`);
+//         }
+//         else{ reject (`error,good afternon`);}
+
+//     })
+// 2
+// } getPromise ()
+// .then((data)=> {document.write(data);})
+// .catch((err)=>{document.write(err);})
+// function getNumRiturnPromis(num1, num2) {
+//   return new Promise((resolve, reject) => {
+//     if (num2 % num1 == 0) {
+//         resolve ("yes")
+//     }
+//     else{
+//         reject("no")
+//     }
+//   });
+// }
+// getNumRiturnPromis(5,1)
+// .then((data)=>{ document.write(data);})
+// .catch((err)=>{document.write(err);})
+// 5
+// /
